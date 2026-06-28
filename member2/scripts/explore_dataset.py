56ebd4aa4508e444fcc47392cb855825ab4a8641
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-28 19:01:14 +0530

    wrapping up day one

---
+++ member2/scripts/explore_dataset.py
@@ -0,0 +1,24 @@
+import json
+
+file_path = "../../dataset/candidates.jsonl"
+
+with open(file_path, "r", encoding="utf-8") as file:
+    first_candidate = json.loads(file.readline())
+
+print(type(first_candidate))
+print(first_candidate)
+
+print("\nProfile Fields\n")
+
+for key in first_candidate["profile"]:
+    print("-", key)
+
+print("\nRedRob Signals\n")
+
+for key in first_candidate["redrob_signals"]:
+    print("-", key)
+
+print(first_candidate["skills"][0])
+
+
+print(first_candidate["career_history"][0])
